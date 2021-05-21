13796c2e6a7ddcda556dc9381f9b01f34b3c00c9
jakajavornik <84388930+jakajavornik@users.noreply.github.com>
2021-05-21 13:33:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ provider "vsphere" {
 data "vsphere_datacenter" "dc" {
   name = var.vsphere_datacenter
 }
-
+/*
 data "vsphere_datastore" "datastore" {
   name          = var.vsphere_datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-/*
+
 data "vsphere_resource_pool" "pool" {
   name          = var.vsphere_resource_pool
   datacenter_id = data.vsphere_datacenter.dc.id
